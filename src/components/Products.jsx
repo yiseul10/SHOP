e729571e03670f69e0614034158165aee83c9850
김이슬 <yiseul10@gmail.com>
2021-11-09 11:45:04 +0900

    데이터 받아오기

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,9 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 import styled from "styled-components";
-import { popularProducts } from "../data";
 import Product from "./Product";
+// import { popularProducts } from "../data";
 import { media } from "../responsive";
 
 const Container = styled.div`
@@ -15,11 +18,45 @@ const Container = styled.div`
   })}
 `;
 
-const Products = () => {
+const Products = ({ cat, filters, sort }) => {
+  const [products, setProduct] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    return () => {
+      cleanup;
+    };
+  }, [cat]);
+
+  const [products, setProduct] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        setError(null);
+        setLoading(true);
+
+        const response = await axios({
+          method: "GET",
+          url: `http://pvpvpvpvp.gonetis.com:8080/sample/products`
+        });
+        setProduct(response.data);
+        console.log(response.data);
+        // 데이터는 response.data 안에 들어있습니다.
+      } catch (e) {
+        setError(e);
+      }
+      setLoading(false);
+    };
+    fetchUsers();
+  }, []);
+  if (!products) return null;
+
   return (
     <Container>
-      {popularProducts.map(item => (
-        <Product item={item} key={item.id} />
+      {products.products.map(item => (
+        <Product item={item} key={[item.index]} />
       ))}
     </Container>
   );
