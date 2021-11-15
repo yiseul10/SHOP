32b722365a049aa36233cb6e7d6ee1891861d032
leekoris <ghks2589@naver.com>
2021-11-15 17:40:57 +0900

    리뷰페이지 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,51 @@ import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import Review from "./pages/Review";
-
 import Notice from "./components/Notice";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import customerService from "./pages/customerService";
 
 const App = () => {
   const user = true;
   return (
-    <BrowserRouter>
+   
+   <BrowserRouter>
+   
       <Notice />
+
       <Header />
+   
       <Switch>
         <Route exact path="/">
           <Home />
         </Route>
+   
         <Route path="/products">
           <ProductList />
         </Route>
+   
         <Route path="/products/:id">
           <Product />
         </Route>
+   
         <Route path="/cart">
           <Cart />
           <Login />
         </Route>
+        
+        <Route exact path="/review">
+        <Review />
+        </Route>
+       
+        <Route exact path="/customerService">
+        <customerService />
+        </Route>
+       
+
         <Route path="/login"> {user ? <NavLink to="/" /> : <Login />}</Route>
-        <Route path="/review">{user ? <NavLink to="/" /> : <Review />}</Route>
       </Switch>
+   
       <Footer />
     </BrowserRouter>
   );
