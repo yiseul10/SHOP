08adfd16b8283bdc09bb3b121021dbd637998251
leekoris <ghks2589@naver.com>
2021-11-16 17:16:15 +0900

    에디터 구현중

--- src/pages/ReviewInsert.js
+++ src/pages/ReviewInsert.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import {Editor, EditorState} from 'draft-js';
+import 'draft-js/dist/Draft.css';
+
+
 
 
 const Inserting = styled.div `
@@ -9,17 +13,34 @@ padding: 10rem`
 
 const ReviewInsert = () => {
     
-   
-    
-    
+   //https://www.youtube.com/watch?v=t12a6z090AU
+   const [editorState, setEditorState] = React.useState(
+    () => EditorState.createEmpty(),
+  );
+
+
     return (
         <Inserting>
         <div>       
            
            <h2> 여기는  리뷰 등록을 위한 곳입니다. </h2>
         </div>
+            <hr/>
+
+            <Editor editorState={editorState} onChange={setEditorState} />
+
+
+
+        
+
 
         </Inserting>
+
+
+
+
+
+
     )
 }
  
