dea67e35085de03b75e9d9dcb313c8180cd18537
leekoris <ghks2589@naver.com>
2021-11-17 17:08:30 +0900

    ckeditor 설정

--- src/pages/ReviewInsert.js
+++ src/pages/ReviewInsert.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
 import styled from 'styled-components'
-import {Editor} from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import {EditorState} from 'draft-js';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 
 const MyBlock = styled.div `
     .wrapper-class{
@@ -18,50 +18,54 @@ const MyBlock = styled.div `
   }
 `;
 
+
+
+
+
 const ReviewInsert = () => {
 
-    // useState로 상태관리하기 초기값은 EditorState.createEmpty() EditorState의 비어있는
-    // ContentState 기본 구성으로 새 개체를 반환 => 이렇게 안하면 상태 값을 나중에 변경할 수 없음.
-    const [editorState, setEditorState] = useState(EditorState.createEmpty());
-
-    const onEditorStateChange = (editorState) => {
-        // editorState에 값 설정
-        setEditorState(editorState);
-    };
+ 
 
     return (
         <MyBlock>
-            <Editor
-                // 에디터와 툴바 모두에 적용되는 클래스
-                wrapperClassName="wrapper-class"
-                // 에디터 주변에 적용된 클래스
-                editorClassName="editor"
-                // 툴바 주위에 적용된 클래스
-                toolbarClassName="toolbar-class"
-                // 툴바 설정
-                toolbar={{
-                    // inDropdown: 해당 항목과 관련된 항목을 드롭다운으로 나타낼것인지
-                    list: {
-                        inDropdown: true
-                    },
-                    textAlign: {
-                        inDropdown: true
-                    },
-                    link: {
-                        inDropdown: true
-                    },
-                    history: {
-                        inDropdown: false
-                    }
-                }} placeholder="내용을 작성해주세요."
-                // 한국어 설정
-                localization={{
-                    locale: 'ko'
-                }}
-                // 초기값 설정
-                editorState={editorState}
-                // 에디터의 값이 변경될 때마다 onEditorStateChange 호출
-                onEditorStateChange={onEditorStateChange}/>
+        <h1> 리뷰등록</h1>
+        <h1> 리뷰등록</h1>
+
+        <h1> 리뷰등록</h1>
+        <h1> 리뷰등록</h1>
+        <h1> 리뷰등록</h1>
+        <h1> 리뷰등록</h1>
+        <h1> 리뷰등록</h1>
+        <h1> 리뷰등록</h1>
+
+          <div className='form-wrapper'>
+        <input className="title-input" type='text' placeholder='제목' 
+        
+
+        />
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>Hello from CKEditor 5!</p>"
+          onReady={editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
+      </div>
+      <button className="submit-button">입력</button>
+
+
+
         </MyBlock>
     );
 
