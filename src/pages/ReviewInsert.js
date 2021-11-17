88975e42b942373aa6a6c348113f597a8a1c68f1
leekoris <ghks2589@naver.com>
2021-11-17 16:49:32 +0900

    리뷰 등록을 위한 텍스트 에디터 작성중

--- src/pages/ReviewInsert.js
+++ src/pages/ReviewInsert.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react'
 import styled from 'styled-components'
-import {Editor, EditorState} from 'draft-js';
-import 'draft-js/dist/Draft.css';
+import {Editor} from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import {EditorState} from 'draft-js';
 
 const MyBlock = styled.div `
     .wrapper-class{
         width: 50%;
         margin: 0 auto;
         margin-bottom: 4rem;
-
     }
   .editor {
     height: 500px !important;
@@ -29,45 +29,42 @@ const ReviewInsert = () => {
         setEditorState(editorState);
     };
 
-        return (
+    return (
+        <MyBlock>
+            <Editor
+                // 에디터와 툴바 모두에 적용되는 클래스
+                wrapperClassName="wrapper-class"
+                // 에디터 주변에 적용된 클래스
+                editorClassName="editor"
+                // 툴바 주위에 적용된 클래스
+                toolbarClassName="toolbar-class"
+                // 툴바 설정
+                toolbar={{
+                    // inDropdown: 해당 항목과 관련된 항목을 드롭다운으로 나타낼것인지
+                    list: {
+                        inDropdown: true
+                    },
+                    textAlign: {
+                        inDropdown: true
+                    },
+                    link: {
+                        inDropdown: true
+                    },
+                    history: {
+                        inDropdown: false
+                    }
+                }} placeholder="내용을 작성해주세요."
+                // 한국어 설정
+                localization={{
+                    locale: 'ko'
+                }}
+                // 초기값 설정
+                editorState={editorState}
+                // 에디터의 값이 변경될 때마다 onEditorStateChange 호출
+                onEditorStateChange={onEditorStateChange}/>
+        </MyBlock>
+    );
 
-            <MyBlock>
-                <Editor
-                    // 에디터와 툴바 모두에 적용되는 클래스
-                    wrapperClassName="wrapper-class"
-                    // 에디터 주변에 적용된 클래스
-                    editorClassName="editor"
-                    // 툴바 주위에 적용된 클래스
-                    toolbarClassName="toolbar-class"
-                    // 툴바 설정
-                    toolbar={{
-                        // inDropdown: 해당 항목과 관련된 항목을 드롭다운으로 나타낼것인지
-                        list: {
-                            inDropdown: true
-                        },
-                        textAlign: {
-                            inDropdown: true
-                        },
-                        link: {
-                            inDropdown: true
-                        },
-                        history: {
-                            inDropdown: false
-                        }
-                    }} placeholder="내용을 작성해주세요."
-                    // 한국어 설정
-                    localization={{
-                        locale: 'ko'
-                    }}
-                    // 초기값 설정
-                    editorState={editorState}
-                    // 에디터의 값이 변경될 때마다 onEditorStateChange 호출
-                    onEditorStateChange={onEditorStateChange}/>
-            </MyBlock>
-
-        )
-
-    }
-
+}
 
 export default ReviewInsert
