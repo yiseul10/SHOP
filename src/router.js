46b5c783f3efec10bb1865b6b049d4337d6347dd
박일규 <t5nn123@naver.com>
2021-11-25 17:11:07 +0900

    경로 설정

--- src/router.js
+++ src/router.js
@@ -6,7 +6,6 @@ import {
   ProductList,
   Home,
   Cart,
-  LoginPage,
   Review,
   customerService,
   CustomerCenter,
@@ -14,7 +13,6 @@ import {
   QuestionOne,
   FAQ,
   ReviewInsert,
-  SignupPage,
   MyPage,
 } from "pages";
 //  page폴더 내의 index파일에 export할 페이지들을 한꺼번에 모아놓고 이 페이지들을 import해줄 때 한줄로 편하게 사용하기위함
@@ -32,8 +30,6 @@ export default function Router() {
         <Route exact path="/review" component={Review} />
         <Route exact path="/customerService" component={CustomerCenter} />
         <Route exact path="/customerService/FAQ" component={FAQ} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/signup" component={SignupPage} />
         <Route exact path="/mypage" component={MyPage} />
         <Route
           exact
