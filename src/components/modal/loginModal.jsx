dded1ab0a8ea2d055303e2b2bc394ce62fd4de84
박일규 <t5nn123@naver.com>
2021-11-25 17:12:41 +0900

    로그인 모달 구현

---
+++ src/components/modal/loginModal.jsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import Modal from "react-modal";
+import React from "react";
+import { IoIosClose } from "react-icons/io";
+
+export function LoginModal({ isVisible, isModalClose, components }) {
+  //  치환 개념
+
+  return (
+    <StyleModal isOpen={isVisible}>
+      <Container>
+        <button onClick={isModalClose}>
+          <IoIosClose />
+        </button>
+        {components}
+      </Container>
+    </StyleModal>
+  );
+}
+
+const StyleModal = styled(Modal)`
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(29, 49, 65, 0.8);
+  overflow-x: hidden;
+  overflow-y: auto;
+  z-index: 1031;
+`;
+
+const Container = styled.div`
+  position: absolute;
+  right: 30%;
+  left: 30%;
+  top: 10%;
+  width: 40%;
+  height: 80%;
+  background-color: #fff;
+  border-radius: 20px;
+  border: solid 10px #fff200;
+  padding: 16px;
+`;
