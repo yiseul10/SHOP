fce14cd084c96c259979b6b6e68d16bf0fd33d2a
박일규 <t5nn123@naver.com>
2021-12-03 16:02:22 +0900

    비밀번호 찾기 페이지

---
+++ src/pages/passwordSearchPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+export function PasswordSearchPage() {
+  return <Cover>비밀번호찾기</Cover>;
+}
+
+const Cover = styled.div`
+  div {
+  }
+`;
