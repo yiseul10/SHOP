09191fe1a279d50869d50ece79220b84a1418cc5
박일규 <t5nn123@naver.com>
2021-12-09 16:38:51 +0900

    setting

--- src/router.js
+++ src/router.js
@@ -19,6 +19,8 @@ import {
 import { OrderListPage } from "pages/ordeListPage";
 import { OrderReviewPage } from "pages/orderReviewPage";
 import { PasswordSearchPage } from "pages/passwordSearchPage";
+import { PasswordResetPage } from "pages/passwordResetPage";
+
 //  page폴더 내의 index파일에 export할 페이지들을 한꺼번에 모아놓고 이 페이지들을 import해줄 때 한줄로 편하게 사용하기위함
 
 export default function Router() {
@@ -30,6 +32,7 @@ export default function Router() {
         <Route exact path="/" component={Home} />
         <Route exact path="/loading" component={LoadingPage} />
         <Route exact path="/passwordsearch" component={PasswordSearchPage} />
+        <Route exact path="/passwordreset" component={PasswordResetPage} />
         <Route exact path="/products/:id" component={Product} />
         <Route exact path="/products" component={ProductList} />
         <Route exact path="/cart" component={Cart} />
