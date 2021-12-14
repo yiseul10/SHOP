7501ff43e22bf3ec133a607abdc9987400e19307
박일규 <t5nn123@naver.com>
2021-12-14 15:46:44 +0900

    비밀번호 찾기 페이지

--- src/pages/passwordSearchPage.jsx
+++ src/pages/passwordSearchPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { Input } from "reactstrap";
 import styled from "styled-components";
-import { EmailCheckInput } from "components/input/emailCheckInput";
+// import { EmailCheckInput } from "components/input/emailCheckInput";
 
 export function PasswordSearchPage() {
   const [email, setEamil] = useState(""); //  이메일
@@ -9,22 +10,45 @@ export function PasswordSearchPage() {
     setEamil(event.target.value);
   }
   function onEmailBtn() {
-    console.log("test");
+    console.log("전송되었습니다");
   }
   return (
     <Cover>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <div className="checkemailbtn">
-        <EmailCheckInput
-          email="email"
+        <Input
+          className="addressInput"
+          id="address"
           onChange={onEmailChange}
-          placeholder="이메일 형식에 맞게 작성해주세요"
-          btn="true"
-          btnName="전송"
-          btnClick={onEmailBtn}
+          placeholder="Email"
         />
+        <button type="button" onClick={onEmailBtn}>
+          전송
+        </button>
       </div>
     </Cover>
   );
 }
 
-const Cover = styled.div``;
+const Cover = styled.div`
+  div {
+    width: 50%;
+    display: flex;
+    margin: auto;
+    .form-control {
+      width: 100%;
+    }
+    button {
+      width: 10%;
+      border: none;
+      border-radius: 10px;
+      background-color: #2cb5e8;
+      color: #fff;
+    }
+  }
+`;
