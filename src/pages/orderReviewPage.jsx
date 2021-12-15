2657cbc68aad65d5011ba34f8041bed7655cdcb7
박일규 <t5nn123@naver.com>
2021-12-15 14:31:00 +0900

    구매후기 페이지 연결

--- src/pages/orderReviewPage.jsx
+++ src/pages/orderReviewPage.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 export function OrderReviewPage() {
-  return <Cover>구매후기페이지</Cover>;
+  return (
+    <Cover>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      구매후기페이지
+    </Cover>
+  );
 }
 
 const Cover = styled.div`
