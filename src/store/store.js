0e724671f386a257c1bc5ec5205a6a551404d9e5
박일규 <t5nn123@naver.com>
2021-12-20 13:42:25 +0900

    redux 주소 받아오기

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './cart-slice';
-import wishReducer from './wish-slice';
-import dataReducer from './api-call';
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./cart-slice";
+import wishReducer from "./wish-slice";
+import dataReducer from "./api-call";
+import addressReducer from "./addressReduce";
 
 export default configureStore({
   reducer: {
     cart: cartReducer,
     wish: wishReducer,
-    products: dataReducer
-  }
+    products: dataReducer,
+    address: addressReducer,
+  },
 });
