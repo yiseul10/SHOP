cd73041f99ab067f91031ceaf594c52da8feea85
pvpvpvpvp <77872841+pvpvpvpvp@users.noreply.github.com>
2021-12-21 17:49:56 +0900

    그림판 추가작업

--- src/pages/Custom.jsx
+++ src/pages/Custom.jsx
@@ -1,23 +1,38 @@
 import styled from "styled-components";
-import React, { useState } from "react";
+import React, { useState ,useRef} from "react";
 
 export function Custom(){
     const [result, setResult] = useState("");
+    const img = useRef();
 
     //   const iframeElement = document.getElementById("browserboard");
-    //   function sendBrowserboardMessage(message) {
-    //     iframeElement.contentWindow.postMessage(message, "https://browserboard.com");
-    //   }
+    
+      const iFrameElement = img && img.current;
+      function sendBrowserboardMessage(message) {        
+        console.log("act");
+        iFrameElement.contentWindow.postMessage(message, "https://browserboard.com");
+      }
+      window.addEventListener("message", (e) => {
+        switch (e.data.action) {
+          case "updateDownloadImageProgress":
+            console.log(e.data);
+            break; /* handle the action */
+            case "downloadImageComplete":
+            console.log(e.data);
+            break; /* handle the action */
+          /* the rest of this file will explain what actions exist and how to handle them. */
+        }
+      });
 
     return(<>
-        <Cover id="browserboard" src="https://browserboard.com/whiteboard/8JU2aujSPEPVhJMfAKdHZP?key=HdhMiwDGmWoUoFjz6DyxzW">
+        <Cover ref={img} src="https://browserboard.com/whiteboard/8JU2aujSPEPVhJMfAKdHZP?key=HdhMiwDGmWoUoFjz6DyxzW">
         </Cover>
-        {/* <button onClick={sendBrowserboardMessage({ "action": "downloadImage" })}>버으튼!</button> */}
+        <button onClick={sendBrowserboardMessage({ "action": "downloadImage" })}>버으튼!</button>
         </>
     )
 }
 
 const Cover = styled.iframe`
-  width: 50%;
-  height: 600px
+  width: 100%;
+  height: 500px
 `;
---
+++ src/pages/CustomerCenter.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const CustSc = styled.div`
+  margin: 70px;
+  
+`;
+
+export const CustomerCenter = () => {
+  return (
+    <CustSc>
+     
+
+       <h1> 공지사항 게시판 들어갈 자리</h1>
+        <h1> 렌더링이 전혀되지않음</h1> 
+     
+      
+    </CustSc>
+  );
+}
